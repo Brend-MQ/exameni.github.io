e42a8247eead9e1ca9183eaab2087e4948190dc0
breand MQ <88468219+Brend-MQ@users.noreply.github.com>
2023-04-30 19:07:04 -0600

    js

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const id_tbn_a = document.getElementById("btn-a")
 const container = document.querySelector("#container-r")
 let val=false
 var x = window.matchMedia("(max-width: 1000px)")
-const carouselExampleIndicators = document.getElementById("carouselExampleIndicators")
+const carouselExampleIndicators = document.getElementById
+("carouselExampleIndicators")
+
+const $aside = document.getElementById("aside")
+const header = document.querySelector("header")
+let nc=false
+
+
 id_tbn_a.addEventListener("click",e=>{
     e.preventDefault()
     if(val!=true){
@@ -18,11 +25,16 @@ id_tbn_a.addEventListener("click",e=>{
     }
 })
 function mediaScreen(x,w){
-
+    const header = document.querySelector("header")
     if(!x.matches){
         container.setAttribute("class","container-r")
         container.style=""
         id_tbn_a.innerHTML =`<a href="#">          <i class="bi bi-three-dots"></i></a>`
+        nc=false
+    }
+    else{
+        console.log(nc)
+        nc=true
     }
         document.querySelector(".main").style=`margin-left:50px;margin-right:10px;width:${w-60}px`
     carouselExampleIndicators.style.width=`${w-60}px`
@@ -34,3 +46,20 @@ window.addEventListener("resize",e=>{
    
 })
 
+window.addEventListener("scroll",e=>{
+
+    if( window.scrollY >= 100){
+        $aside.style.top = "0"
+        $aside.style.transition = "0.3s"
+        container.style=`poition:fixed;top:0px;`
+    }
+    else{
+        console.log(nc)
+      if(nc){
+        container.style=`poition:fixed;top:${header.clientHeight}px;`
+        console.log("asdf")
+    }
+        $aside.style.top=`${header.clientHeight}px`
+    }
+
+})
